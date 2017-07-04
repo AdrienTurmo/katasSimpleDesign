ae73fba140a3d158ca3a46adf470a0366aab7550
Adrien Turmo <adrien.turmo@gmail.com>
2017-07-04 10:59:55 +0200

    FizzBuzz : refacto : replace if by modulo 3

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,7 @@ package fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzzOf(int number) {
-        if (number == 3) {
-            return "Fizz";
-        }
-        if (number == 6) {
+        if (number % 3 == 0) {
             return "Fizz";
         }
         return Integer.toString(number);
