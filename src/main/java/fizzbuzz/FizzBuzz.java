c670b739ef4d7c69b465071d380cad2d923b7ed7
Adrien Turmo <adrien.turmo@gmail.com>
2017-07-04 10:55:04 +0200

    FizzBuzz : refacto : replace if by toString method

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -3,13 +3,7 @@ package fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzzOf(int number) {
-        if (number == 1) {
-            return "1";
-        }
-        if (number == 2) {
-            return "2";
-        }
-        return "4";
+        return Integer.toString(number);
     }
 
 }
