1f19156c75c140d0de42732d5aa839d93dea08d6
Adrien Turmo <adrien.turmo@gmail.com>
2017-07-04 11:29:46 +0200

    FizzBuzz : refacto extract methods

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -3,16 +3,34 @@ package fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzzOf(int number) {
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        }
-        if (number % 5 == 0) {
-            return "Buzz";
-        }
+        String resultOfFizzBuzz = "";
+
+        resultOfFizzBuzz += ifDivisibleByThreeReturnFizz(number);
+        resultOfFizzBuzz += ifDivisibleByFiveReturnBuzz(number);
+
+        resultOfFizzBuzz = returnTheNumberIfNotDivisibleByThreeOrFive(number, resultOfFizzBuzz);
+
+        return resultOfFizzBuzz;
+    }
+
+    private static String ifDivisibleByThreeReturnFizz(int number) {
         if (number % 3 == 0) {
             return "Fizz";
         }
-        return Integer.toString(number);
+        return "";
     }
 
+    private static String ifDivisibleByFiveReturnBuzz(int number) {
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return "";
+    }
+
+    private static String returnTheNumberIfNotDivisibleByThreeOrFive(int number, String resultOfFizzBuzz) {
+        if (resultOfFizzBuzz.equals("")) {
+            resultOfFizzBuzz = Integer.toString(number);
+        }
+        return resultOfFizzBuzz;
+    }
 }
